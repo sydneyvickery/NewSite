4f33f4480191acb86dbc0081d55df75213224206
sydneyvickery <sydneyy@gmail.com>
2015-03-10 23:26:03 -0700

    Updates to footer and resume

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function(grunt) {
 	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
 
 		express: {
 		  all: {
